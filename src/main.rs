bfc194dfa748a5e03259cbc1de8480e0995137b6
Ethan Brierley <ethanboxx@gmail.com>
2019-12-17 20:00:36 +0000

    starter code

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+pub struct Pong;
+
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+    // We'll put the rest of the code here.
+
+    Ok(())
+}
